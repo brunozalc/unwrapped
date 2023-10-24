efbe34d586a67af319f5d4c44c6e47a9e5c02f7c
brunozalc <brun0zalc@hotmail.com>
2023-10-24 00:14:29 -0300

    initial commit

---
+++ app/components/IndexTitle.tsx
@@ -0,0 +1,14 @@
+export default function IndexTitle() {
+  return (
+    <div className="hero min-h-screen flex justify-start">
+      <div className="hero-content flex-col lg:flex-row-reverse pl-80">
+        <div>
+          <h1 className="text-8xl text-slate-50 font-bold">unwrapped</h1>
+          <p className="py-6">
+            See your Spotify statistics in a new and simple way.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/components/LoginButton.tsx
@@ -0,0 +1,7 @@
+export default function LoginButton() {
+  return (
+    <button className="bg-green-600 text-slate-50 btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">
+      Login
+    </button>
+  );
+}
