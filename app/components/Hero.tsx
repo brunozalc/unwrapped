0d545a41b8c33ab6c7ade89e636e01fb76073579
brunozalc <brun0zalc@hotmail.com>
2023-10-26 23:35:32 -0300

    deixa mais responsivo ao definir tamanhos para outras telas

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,13 +4,13 @@ import animationData from "../../public/visualizer.json";
 
 const Hero: React.FC = () => {
   return (
-    <div className="hero min-h-screen flex items-center justify-between">
-      <div className="hero-content pl-40">
-        <div className="max-w-md">
-          <h1 className="text-8xl text-slate-50 font-bold animate-fade-in">
+    <div className="hero min-h-screen flex flex-col md:flex-row items-center justify-between relative">
+      <div className="hero-content w-full md:w-1/2 pl-4 md:pl-40 z-10 relative">
+        <div className="max-w-full">
+          <h1 className="text-3xl md:text-8xl text-slate-50 font-bold animate-fade-in">
             unwrapped
           </h1>
-          <p className="py-6 animate-fade-in">
+          <p className="text-sm md:text-base py-2 md:py-6 animate-fade-in">
             See your Spotify statistics in a new and simple way.
           </p>
           <button className="bg-green-600 text-slate-50 btn btn-xs sm:btn-sm md:btn-md lg:btn-lg mt-4 animate-fade-in">
@@ -18,11 +18,15 @@ const Hero: React.FC = () => {
           </button>
         </div>
       </div>
-      <div className="pr-40">
-        <LottieAnimation animationData={animationData} />
+      <div className="hero-content w-full md:w-1/2 pr-4 md:pr-40 z-0 relative">
+        <div className="max-w-full">
+          <LottieAnimation animationData={animationData} />
+        </div>
       </div>
     </div>
   );
 };
+
+
 
 export default Hero;
