e3776f2dc322f246795ed6bb01f179a87e902041
koeitsu <brun0zalc@hotmail.com>
2023-10-27 19:13:44 -0300

    mocka página de login do spotify

---
+++ app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <div className="bg-offblack min-h-screen  flex justify-center items-center px-4 sm:px-6 md:px-8 lg:px-16 xl:px-32">
+      <h1>Login</h1>
+    </div>
+  );
+}
